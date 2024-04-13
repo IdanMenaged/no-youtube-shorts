88b1c28090a145565e219886d44eab161e6f2c8c
idan menaged <IdanMenaged>
2024-04-13 15:13:09 +0300

    photo input (not functional)

--- frontend/screens/Add.js
+++ frontend/screens/Add.js
@@ -1,9 +1,17 @@
-import { View, StyleSheet, TextInput, TouchableWithoutFeedback, Text } from "react-native"
+import { View, StyleSheet, TextInput, TouchableWithoutFeedback, Text, Image } from "react-native"
+import { launchImageLibrary } from 'react-native-image-picker';
+
+photoInput = require('../assets/photo-input.png')
 
 export default function Add() {
     return (
         <View style={styles.container}>
-            <TextInput placeholder='Name' style={[styles.text, styles.textInput]} />
+            <View style={styles.form}>
+                <TouchableWithoutFeedback>
+                    <Image source={photoInput} style={styles.photoInput} />
+                </TouchableWithoutFeedback>
+                <TextInput placeholder='Name' style={[styles.text, styles.textInput]} />
+            </View>
             <TouchableWithoutFeedback>
                 <View style={styles.submitContainer}>
                     <Text style={[styles.text, styles.submit]}>Submit</Text>
@@ -20,23 +28,24 @@ const styles = StyleSheet.create({
         justifyContent: ' center',
         height: '100%',
         width: '100%',
-        flexDirection: 'column',
         justifyContent: 'space-between'
     },
     text: {
-        width: '60%',
+        width: '600%',
         height: '10%',
         textAlign: 'center',
         borderRadius: 25,
-        marginTop: '25%',
+        // marginTop: '10%',
         fontFamily: 'Ubuntu',
         fontSize: 24
     },
     textInput: {
-        backgroundColor: '#D9D9D9'
+        backgroundColor: '#D9D9D9',
+        width: '60%',
+        height: '15%'
     },
     submit: {
-        height: '100%', // centers it vertically
+        height: '50%', // centers it vertically
     },
     submitContainer: {
         backgroundColor: '#D9D9D9',
@@ -46,6 +55,16 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         marginBottom: '20%'
+    },
+    photoInput: {
+        marginTop: '25%',
+        width: 200,
+        height: 200
+    },
+    form: {
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        width: '100%'
     }
 });
   

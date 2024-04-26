4148b818d86112c1252300613b7ab31b5a24623f
idan menaged <IdanMenaged>
2024-04-26 12:30:04 +0300

    tried to add image (doesn't work

--- frontend/components/VideoPreview.js
+++ frontend/components/VideoPreview.js
@@ -1,13 +1,28 @@
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet, View, Text, Image } from "react-native";
 
 export default function VideoPreview({ searchResult }) {
+    console.log(searchResult.snippet.thumbnails.default.url)
+
     return (
-        <Text style={styles.text}>{searchResult.snippet.title}</Text>
+        <View>
+            <Image
+                source={{ uri: searchResult.snippet.thumbnails.default.url }}
+                // source={require('../assets/icon.png')}
+                style={styles.icon}
+            />
+            <Text style={styles.text}>{searchResult.snippet.title}</Text>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
     text: {
         color: '#ffffff'
+    },
+    icon: {
+        // width: 10000,
+        // height: 10000,
+        // resizeMode: 'contain',
+        backgroundColor: '#ffffff'
     }
 })
